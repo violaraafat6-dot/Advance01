e51aeb1a08483b5b6ae2e404ba026dc8cf253f73
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-29 02:56:46 +0300

    question1

--- Advance01/Program.cs
+++ Advance01/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Advance01
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Advance01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region question1
+
+//            Q1
+//A generic class is a class that works with different data types using a type parameter such as T
+//Generics are used to:
+// -Reuse the same class with different data types
+//-Provide type safety
+// - Avoid boxing/unboxing for value type
+//-Reduce code duplication
+
+
+
+            #endregion
         }
     }
 }
