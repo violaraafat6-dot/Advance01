ece095b9f7ff54ff80243e743604c63767da40c6
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-29 03:35:08 +0300

    question8

--- Advance01/IRepository.cs
+++ Advance01/IRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Advance01;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using static System.Runtime.InteropServices.JavaScript.JSType;
@@ -20,14 +21,51 @@ namespace Advance01
 
     #region question7
 
-//    The struct constraint means that the generic type must be a value type
+    //    The struct constraint means that the generic type must be a value type
 
-//class Example<T> where T : struct
+    //class Example<T> where T : struct
+    //    {
+    //        public T Value { get; set; }
+    //    }
+    ///
+    //Example<int> example = new Example<int>();
+
+
+    #endregion
+
+
+    #region question8
+
+
+//    The class constraint means that the generic type must be a reference type
+
+//class Example<T> where T : class
 //    {
 //        public T Value { get; set; }
+//}
+//Another ex
+
+//public class Repository<T> where T. class
+//    {
+//        public T Item(get; set; }
+//public Repository(T item)
+//        {
+//            Item = item;
+//        }
 //    }
-///
-//Example<int> example = new Example<int>();
+//class Person
+//{
+//    public string Name(get; set; )
+//}
+//class Program
+//{
+//    static void Main()
+//var r1 = new Repository<string>("Hello": );
+//    Var r2 = new
+//    Repository<Person>(new Person { Name = "Al1" });
+//}
+//}
+
 
 
     #endregion
