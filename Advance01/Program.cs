d3a52cc4d66a8bdfa2708c421b6690b07024be10
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-29 03:09:12 +0300

    question2,3

--- Advance01/Program.cs
+++ Advance01/Program.cs
@@ -8,13 +8,20 @@ namespace Advance01
         {
             #region question1
 
-//            Q1
-//A generic class is a class that works with different data types using a type parameter such as T
-//Generics are used to:
-// -Reuse the same class with different data types
-//-Provide type safety
-// - Avoid boxing/unboxing for value type
-//-Reduce code duplication
+            //            Q1
+            //A generic class is a class that works with different data types using a type parameter such as T
+            //Generics are used to:
+            // -Reuse the same class with different data types
+            //-Provide type safety
+            // - Avoid boxing/unboxing for value type
+            //-Reduce code duplication
+
+
+
+            #endregion
+
+            #region question4
+
 
 
 
---
+++ Advance01/container.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advance01
+{
+    #region question2,3
+
+    //class Container<T>
+    //{
+    //    private T value;
+
+    //    public void Add(T value)
+    //    {
+    //        this.value = value;
+    //    }
+
+    //    public T Get()
+    //    {
+    //        return value;
+    //    }
+    //}
+
+
+    //question3
+    //    class Pair<TKey, TValue>
+    //{
+    //    public TKey Key { get; set; }
+    //    public TValue Value { get; set; }
+
+    //    public Pair(TKey key, TValue value)
+    //    {
+    //        Key = key;
+    //        Value = value;
+    //    }
+    //}
+
+    #endregion
+}
